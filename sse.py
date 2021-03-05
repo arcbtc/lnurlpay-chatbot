9b7f66dfe1e46de03699897e0d88c14228456b22
benarc <ben@arc.wales>
2021-03-05 13:53:27 +0000

    dfjdf

--- sse.py
+++ sse.py
@@ -16,7 +16,7 @@ def index():
 @app.route("/message/", methods=["POST"])
 def publish_messages():
     print(request)
-    print(request.args)
+    print(request.args.get("content"))
     sse.publish({"message": "thenote"}, type="greeting")
     return "Message sent!"
 
